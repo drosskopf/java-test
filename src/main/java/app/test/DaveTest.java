e926748ed15680c2db66e98df71ad7dc993fbc6d
David Rosskopf <drosskopf@ldschurch.org>
2018-01-29 08:02:18 -0700

    initial checkin

---
+++ src/main/java/app/test/DaveTest.java
@@ -0,0 +1,41 @@
+package app.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DaveTest {
+    private WebDriver driver;
+
+    @Before
+    public void testState() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void test() throws Exception {
+        // Go to the Google Suggest home page
+        driver.get("http://www.google.com/webhp?complete=1&hl=en");
+
+        // Enter the query string "Cheese"
+        WebElement query = driver.findElement(By.name("q"));
+        query.sendKeys("Cheese" + Keys.ENTER);
+        Assert.assertEquals(driver.getTitle(), "Cheese - Google Search");
+
+    }
+
+    @After
+    public void testCleanUp() throws Exception {
+        driver.quit();
+    }
+}
